85bc45853ba8e15ba4f69f5471e18b3ed4792a36
krisograbek <krzysztof.ograbek@gmail.com>
2022-04-04 11:46:12 +0200

    replace src/ with minimal frontend, setup mongoose

--- server/index.js
+++ server/index.js
@@ -2,3 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// use mongoDB atlas
+const CONNECTION_URL = 'mongodb+srv://kris:kris@cluster0.maxwj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+// deprecated
+// mongoose.set('useFindAndModify', false);
