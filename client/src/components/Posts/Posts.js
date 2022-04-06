c104d753ccfc9d21f6da63afb344ad447cd1e29b
krisograbek <krzysztof.ograbek@gmail.com>
2022-04-06 11:06:10 +0200

    implement logic of Posts.js

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import Post from './Post/Post'
 import useStyles from './styles';
@@ -13,9 +15,15 @@ const Posts = () => {
   console.log('posts', posts)
   return (
     <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
+      {!posts.length ? <CircularProgress /> : (
+        <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+          {posts.map((post) => (
+            <Grid item key={post._id} xs={12} sm={6}>
+              <Post post={post} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </>
   )
 }
