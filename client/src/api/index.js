88751829daaa824918d685540b1dafa5d72d40dc
krisograbek <krzysztof.ograbek@gmail.com>
2022-04-13 12:47:02 +0200

    use baseURL for axios

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
-const postsURL = 'http://localhost:5000/posts';
-const usersURL = 'http://localhost:5000/users';
+const API = axios.create({ baseURL: 'http://localhost:5000/' })
 
-export const fetchPosts = () => axios.get(postsURL);
-export const createPost = (newPost) => axios.post(postsURL, newPost);
-export const updatePost = (id, updatedPost) => axios.patch(`${postsURL}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${postsURL}/${id}`);
-export const incrementLikes = (id) => axios.patch(`${postsURL}/${id}/likePost`) // we have to add /likePost to diffentiate from update above
+export const fetchPosts = () => API.get('posts');
+export const createPost = (newPost) => API.post('posts', newPost);
+export const updatePost = (id, updatedPost) => API.patch(`posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`posts/${id}`);
+export const incrementLikes = (id) => API.patch(`posts/${id}/likePost`) // we have to add /likePost to diffentiate from update above
 
 // users
-export const signup = (formData) => axios.post(`${usersURL}/signup`, formData);
-export const signin = (formData) => axios.post(`${usersURL}/signin`, formData);
+export const signup = (formData) => API.post(`users/signup`, formData);
+export const signin = (formData) => API.post(`users/signin`, formData);
